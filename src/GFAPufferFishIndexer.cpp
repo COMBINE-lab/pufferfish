c391ca917c38ea1d2ce2877fbf4067e410394a7f
Rob Patro <rob@newton>
2017-06-16 12:24:49 -0400

    remove some unnecessary code

--- src/GFAPufferFishIndexer.cpp
+++ src/GFAPufferFishIndexer.cpp
@@ -215,9 +215,6 @@ int pufferfishIndex(util::IndexOptions& indexOpts) {
   std::cerr << "rankSize = " << sdsl::size_in_mega_bytes(rankVec) << "\n";
   //std::cerr << "posSize = " << sdsl::size_in_mega_bytes(posVec) << "\n";
   std::cerr << "num keys = " << nkeys << "\n";
-  typedef boomphf::SingleHashFunctor<uint64_t> hasher_t;
-  typedef boomphf::mphf<uint64_t, hasher_t> boophf_t;
-
   ContigKmerIterator kb(&seqVec, &rankVec, k, 0);
   ContigKmerIterator ke(&seqVec, &rankVec, k, seqVec.size() - k + 1);
 
@@ -229,6 +226,9 @@ int pufferfishIndex(util::IndexOptions& indexOpts) {
   }
   std::cerr << "num keys (iterator)= " << nkeyIt << "\n";
 #endif //PUFFER_DEBUG
+
+  typedef boomphf::SingleHashFunctor<uint64_t> hasher_t;
+  typedef boomphf::mphf<uint64_t, hasher_t> boophf_t;
 
   auto keyIt = boomphf::range(kb, ke);
   boophf_t* bphf = new boophf_t(nkeys, keyIt, 16, 2.5); // keys.size(), keys, 16);
@@ -290,129 +290,4 @@ int pufferfishIndex(util::IndexOptions& indexOpts) {
   hstream.close();
 
    return 0;
-
-  size_t N = nkeys; // keys.size();
-  size_t S = seqVec.size();
-  size_t found = 0;
-  size_t notFound = 0;
-  size_t correctPosCntr = 0;
-  size_t incorrectPosCntr = 0;
-  size_t numTrueTxp = 0;
-  {
-    std::vector<std::string> rankOrderedContigIDs;
-
-    auto& cnmap = pf.getContigNameMap();
-    for (auto& kv : cnmap) {
-      rankOrderedContigIDs.push_back(kv.first);
-    }
-    auto& trRefIDs = pf.getRefIDs();
-    ScopedTimer st;
-    fastx_parser::FastxParser<fastx_parser::ReadSeq> parser(read_file, 1, 1);
-    parser.start();
-    // Get the read group by which this thread will
-    // communicate with the parser (*once per-thread*)
-    size_t rn{0};
-    size_t kmer_pos{0};
-    auto rg = parser.getReadGroup();
-    sdsl::bit_vector::rank_1_type realRank(&rankVec);
-    sdsl::bit_vector::select_1_type realSelect(&rankVec);
-    while (parser.refill(rg)) {
-      // Here, rg will contain a chunk of read pairs
-      // we can process.
-      for (auto& rp : rg) {
-        kmer_pos = 0;
-        if (rn % 500000 == 0) {
-          std::cerr << "rn : " << rn << "\n";
-          std::cerr << "found = " << found << ", notFound = " << notFound
-                    << "\n";
-        }
-        ++rn;
-        auto& r1 = rp.seq;
-        CanonicalKmer mer;
-        bool merOK = mer.fromStr(r1); // mer.from_chars(r1.begin());
-        if (!merOK) {
-          std::cerr << "contig too short!";
-          std::exit(1);
-        }
-        auto km = mer.getCanonicalWord();
-        size_t res = bphf->lookup(km);
-        uint64_t pos =
-            (res < N) ? posVec[res] : std::numeric_limits<uint64_t>::max();
-        if (pos <= S - k) {
-          uint64_t fk = seqVec.get_int(2 * pos, 2 * k);
-          my_mer fkm;
-          fkm.word__(0) = fk;
-          if (mer.fwWord() == fkm.word(0) or mer.rcWord() == fkm.word(0)) {
-            found += 1;
-            bool correctPos = false;
-            bool foundTxp = false;
-            auto rank = realRank(pos);
-            for (auto& tr : pf.contig2pos[rankOrderedContigIDs[rank]]) {
-              if (trRefIDs[tr.transcript_id()] == rp.name) {
-                foundTxp = true;
-                uint64_t sp = (uint64_t)realSelect(rank);
-                auto relPos = pos - sp;
-                auto clen = (uint64_t)realSelect(rank + 1) - sp;
-                if (relPos + tr.pos() == kmer_pos or
-                    clen - (relPos - tr.pos() - k - 1) == kmer_pos) {
-                  correctPos = true;
-                  break;
-                } else {
-                  // std::cerr << "ours : " << relPos + tr.pos << " , true : "
-                  // << kmer_pos << "\n";
-                }
-              }
-            }
-            if (correctPos) {
-              correctPosCntr++;
-            } else {
-              incorrectPosCntr++;
-            }
-            if (foundTxp) {
-              numTrueTxp++;
-            }
-            // if (!foundTxp) { std::cerr << "failed to find true txp [" <<
-            // rp.name << "]\n"; }
-          } else {
-            // std::cerr << "rn = " << rn - 1 << ", fk = " <<
-            // fkm.get_canonical().to_str() << ", km = " <<
-            // mer.get_canonical().to_str() << ", pkmer = " << pkmer <<" \n";
-            // std::cerr << "found = " << found << ", not found = " << notFound
-            // << "\n";
-            notFound += 1;
-          }
-        } else {
-          // std::cerr << "pos = " << pos << ", shouldn't happen\n";
-          notFound += 1;
-        }
-
-        for (size_t i = k; i < r1.length(); ++i) {
-          mer.shiftFw(r1[i]);
-          km = mer.getCanonicalWord();
-          res = bphf->lookup(km);
-          pos = (res < N) ? posVec[res] : std::numeric_limits<uint64_t>::max();
-          if (pos <= S - k) {
-            uint64_t fk = seqVec.get_int(2 * pos, 62);
-            my_mer fkm;
-            fkm.word__(0) = fk; // fkm.canonicalize();
-            if (mer.fwWord() == fkm.word(0) or mer.rcWord() == fkm.word(0)) {
-              found += 1;
-
-            } else {
-              notFound += 1;
-            }
-          } else {
-            // std::cerr << "pos = " << pos << ", shouldn't happen\n";
-            notFound += 1;
-          }
-        }
-      }
-    }
-  }
-  std::cerr << "found = " << found << ", not found = " << notFound << "\n";
-  std::cerr << "correctPos = " << correctPosCntr
-            << ", incorrectPos = " << incorrectPosCntr << "\n";
-  std::cerr << "corrextTxp = " << numTrueTxp << "\n";
-
-  return 0;
 }
