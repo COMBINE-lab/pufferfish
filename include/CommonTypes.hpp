80065364c963b64dfd7e809c490a688533d738d5
Rob Patro <rob@newton>
2017-10-26 14:27:51 -0400

    add CommonTypes

---
+++ include/CommonTypes.hpp
@@ -0,0 +1,10 @@
+#ifndef __PUFFERFISH_COMMON_TYPES_HPP__
+#define __PUFFERFISH_COMMON_TYPES_HPP__
+
+namespace pufferfish {
+  namespace common_types {
+    using ReferenceID = size_t;
+  }
+}
+
+#endif // __PUFFERFISH_COMMON_TYPES_HPP__
