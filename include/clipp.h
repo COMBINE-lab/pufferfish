400c1f86236b5ec979c16a23e0ca1126ec30f9e4
Rob Patro <rob@newton>
2017-12-07 10:39:42 -0500

    update clipp

--- include/clipp.h
+++ include/clipp.h
@@ -343,7 +343,7 @@ template<>
 struct make<unsigned char> {
     static inline unsigned char from(const char* s) {
         if(!fwd_to_unsigned_int(s)) return (0);
-        return clamped_on_limits<unsigned char>(std::stoull(s));
+        return clamped_on_limits<unsigned char>(std::strtoull(s,nullptr,10));
     }
 };
 
@@ -351,7 +351,7 @@ template<>
 struct make<unsigned short int> {
     static inline unsigned short int from(const char* s) {
         if(!fwd_to_unsigned_int(s)) return (0);
-        return clamped_on_limits<unsigned short int>(std::stoull(s));
+        return clamped_on_limits<unsigned short int>(std::strtoull(s,nullptr,10));
     }
 };
 
@@ -359,7 +359,7 @@ template<>
 struct make<unsigned int> {
     static inline unsigned int from(const char* s) {
         if(!fwd_to_unsigned_int(s)) return (0);
-        return clamped_on_limits<unsigned int>(std::stoull(s));
+        return clamped_on_limits<unsigned int>(std::strtoull(s,nullptr,10));
     }
 };
 
@@ -367,7 +367,7 @@ template<>
 struct make<unsigned long int> {
     static inline unsigned long int from(const char* s) {
         if(!fwd_to_unsigned_int(s)) return (0);
-        return clamped_on_limits<unsigned long int>(std::stoull(s));
+        return clamped_on_limits<unsigned long int>(std::strtoull(s,nullptr,10));
     }
 };
 
@@ -375,7 +375,7 @@ template<>
 struct make<unsigned long long int> {
     static inline unsigned long long int from(const char* s) {
         if(!fwd_to_unsigned_int(s)) return (0);
-        return clamped_on_limits<unsigned long long int>(std::stoull(s));
+        return clamped_on_limits<unsigned long long int>(std::strtoull(s,nullptr,10));
     }
 };
 
@@ -386,56 +386,56 @@ struct make<char> {
         const auto n = std::strlen(s);
         if(n == 1) return s[0];
         //parse as integer
-        return clamped_on_limits<char>(std::stoll(s));
+        return clamped_on_limits<char>(std::strtoll(s,nullptr,10));
     }
 };
 
 template<>
 struct make<short int> {
     static inline short int from(const char* s) {
-        return clamped_on_limits<short int>(std::stoll(s));
+        return clamped_on_limits<short int>(std::strtoll(s,nullptr,10));
     }
 };
 
 template<>
 struct make<int> {
     static inline int from(const char* s) {
-        return clamped_on_limits<int>(std::stoll(s));
+        return clamped_on_limits<int>(std::strtoll(s,nullptr,10));
     }
 };
 
 template<>
 struct make<long int> {
     static inline long int from(const char* s) {
-        return clamped_on_limits<long int>(std::stoll(s));
+        return clamped_on_limits<long int>(std::strtoll(s,nullptr,10));
     }
 };
 
 template<>
 struct make<long long int> {
     static inline long long int from(const char* s) {
-        return (std::stoll(s));
+        return (std::strtoll(s,nullptr,10));
     }
 };
 
 template<>
 struct make<float> {
     static inline float from(const char* s) {
-        return (std::stof(s));
+        return (std::strtof(s,nullptr));
     }
 };
 
 template<>
 struct make<double> {
     static inline double from(const char* s) {
-        return (std::stod(s));
+        return (std::strtod(s,nullptr));
     }
 };
 
 template<>
 struct make<long double> {
     static inline long double from(const char* s) {
-        return (std::stold(s));
+        return (std::strtold(s,nullptr));
     }
 };
 
@@ -3857,24 +3857,38 @@ public:
                 //discard other alternatives
                 match.pos_.skip_alternatives();
             }
-            //if current param is not repeatable, go directly to next
-            if(!match->repeatable() && !match->is_group()) {
-                curMatched_ = false;
-                ++match.pos_;
-            }
-            else {
-                curMatched_ = true;
-            }
 
-            if(match.pos_.level() > pos_.level()) {
-                scopes_.push(pos_.undo_point());
-                pos_ = std::move(match.pos_);
+            if(is_last_in_current_scope(match.pos_)) {
+                //if current param is not repeatable -> back to previous scope
+                if(!match->repeatable() && !match->is_group()) {
+                    curMatched_ = false;
+                    pos_ = std::move(match.pos_);
+                    if(!scopes_.empty()) pos_.undo(scopes_.top());
+                }
+                else { //stay at match position
+                    curMatched_ = true;
+                    pos_ = std::move(match.pos_);
+                }
             }
-            else if(match.pos_.level() < pos_.level()) {
-                return_to_level(match.pos_.level());
-            }
-            else {
-                pos_ = std::move(match.pos_);
+            else { //not last in current group
+                //if current param is not repeatable, go directly to next
+                if(!match->repeatable() && !match->is_group()) {
+                    curMatched_ = false;
+                    ++match.pos_;
+                } else {
+                    curMatched_ = true;
+                }
+
+                if(match.pos_.level() > pos_.level()) {
+                    scopes_.push(pos_.undo_point());
+                    pos_ = std::move(match.pos_);
+                }
+                else if(match.pos_.level() < pos_.level()) {
+                    return_to_level(match.pos_.level());
+                }
+                else {
+                    pos_ = std::move(match.pos_);
+                }
             }
             posAfterLastMatch_ = pos_;
         }
@@ -3888,6 +3902,16 @@ public:
     }
 
 private:
+    //-----------------------------------------------------
+    bool is_last_in_current_scope(const dfs_traverser& pos)
+    {
+        if(scopes_.empty()) return pos.is_last_in_path();
+        //check if we would leave the current scope on ++
+        auto p = pos;
+        ++p;
+        return p.level() < scopes_.top().level();
+    }
+
     //-----------------------------------------------------
     void check_repeat_group_start(const scoped_dfs_traverser& newMatch)
     {
@@ -6107,6 +6131,16 @@ inline doc_string doc_label(const parameter& p)
     if(!p.flags().empty()) return p.flags().front();
     if(!p.label().empty()) return p.label();
     return doc_string{"<?>"};
+}
+
+inline doc_string doc_label(const group&)
+{
+    return "<group>";
+}
+
+inline doc_string doc_label(const pattern& p)
+{
+    return p.is_group() ? doc_label(p.as_group()) : doc_label(p.as_param());
 }
 
 
