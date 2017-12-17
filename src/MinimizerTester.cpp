fd42b6aa1d828d26686d4df905cb04da55faf0e4
Fatemeh <sfmodarres@gmail.com>
2017-12-17 08:54:15 -0500

    Were not multiplying bitsPerPos by #kmers. Corrected that.

--- src/MinimizerTester.cpp
+++ src/MinimizerTester.cpp
@@ -10,7 +10,7 @@ struct Bucket {
   uint64_t numOfUnitigs = 0;
   uint64_t seqLength = 0;
 };
-void findMinimizer(uint64_t kmer, uint8_t k, uint8_t m, uint8_t* minimizer, uint64_t* minimizerPos) {
+void findMinimizer(uint64_t kmer, uint8_t k, uint8_t m, uint64_t* minimizer, uint64_t* minimizerPos) {
   uint8_t mask = (0x01 << 2*m)-1;
   for (uint64_t cntr = 0; cntr < k; cntr++) {
     //std::cout << "c" << cntr << " " << ((kmer >> 2*cntr) & mask) << " ";
@@ -24,17 +24,17 @@ void findMinimizer(uint64_t kmer, uint8_t k, uint8_t m, uint8_t* minimizer, uint
 int main(int argc, char* argv[]) {
 
   PufferfishIndex pfi(argv[1]);
-  std::cout << "Successfully read.\n";
   uint8_t k = pfi.k();
   sdsl::int_vector<2>& seq = pfi.getSeq();
   sdsl::bit_vector::select_1_type& unitigS = pfi.getBoundarySelect();
 
   for (uint8_t m = 2; m < 6; m++) {
+    std::cout << "\n\nMINIMIZER LENGTH : "<< static_cast<size_t>(m) << "\n\n";
     uint64_t contigCntr = 1;
     uint64_t start = 0;
     uint64_t cur = 0;
     uint64_t next = unitigS(contigCntr);
-    uint8_t minimizer = 0x01 << 2*m;
+    uint64_t minimizer = 0x01 << 2*m;
     uint64_t minimizerPos = 0;
     uint64_t totalNumOfKmers = (next - cur) -k+1;
 
@@ -58,25 +58,28 @@ int main(int argc, char* argv[]) {
         buckets[minimizer].seqLength += (cur+(k-1)-prevPos);
         buckets[minimizer].numOfKmers += (cur+(k-1)-prevPos) - k+1;
         buckets[minimizer].numOfUnitigs++;
-        minimizer = 0x01 << 2*m; // reset minimizer to max for next contig
-        start = next;
-        cur = next;
-        prevPos = cur;
-        next = unitigS(contigCntr++);
-        if (next == 0) break;
-        totalNumOfKmers += next-cur-k+1;
         if (tmpUnitigSplitCnt > 0) {
           splittedUnitigCnt++;
           unitigSplitCnt += tmpUnitigSplitCnt;
           // reset current unitig split cnt to zero
           tmpUnitigSplitCnt = 0;
         }
-      }
+        minimizer = 0x01 << 2*m; // reset minimizer to max for next contig
+        start = next;
+        cur = next;
+        prevPos = cur;
+        next = unitigS(contigCntr++);
+        if (next == 0) {
+          std::cout << cur << " " << seq.size() << "\n";
+          break;
+        }
+        totalNumOfKmers += next-cur-k+1;
+     }
 
       // if current kmer is the first kmer in unitig or we've surpassed current minimizer
       if (cur == start or cur > minimizerPos) {
         uint64_t kmer = seq.get_int(2*cur, 2*k);
-        uint8_t tmpMinimizer = 0x01 << 2*m;
+        uint64_t tmpMinimizer = 0x01 << 2*m;
         uint64_t relMinimizerPos = 0;
         findMinimizer(kmer, k, m, &tmpMinimizer, &relMinimizerPos);
         minimizerPos = cur + relMinimizerPos;
@@ -93,7 +96,7 @@ int main(int argc, char* argv[]) {
         //std::cout << (size_t)minimizer << "\n";
       }
       else {
-        uint8_t nextPotentialMinimizer = seq.get_int(2*(cur+(k-m)), 2*m);
+        uint64_t nextPotentialMinimizer = seq.get_int(2*(cur+(k-m)), 2*m);
         if (nextPotentialMinimizer < minimizer) {
           tmpUnitigSplitCnt++;
           minimizer = nextPotentialMinimizer;
@@ -107,7 +110,6 @@ int main(int argc, char* argv[]) {
       cur++;
     }
     uint64_t totalBits = totalNumOfKmers*ceil(log2(totalNumOfKmers)) + (seq.size()*2);
-    std::cout << "\nMINIMIZER LENGTH : "<< static_cast<size_t>(m) << "\n\n";
     std::cout << "# of unitigs before splitting: " << contigCntr << "\n"
               << "# of unitigs after splitting: " << unitigSplitCnt << "\n"
               << "# of unitigs that were splitted: " << splittedUnitigCnt << "\n";
@@ -118,18 +120,21 @@ int main(int argc, char* argv[]) {
 
 
     totalBits = 0;
+    uint64_t sumNumKmers = 0;
     std::cout << "\nAfter applying minimizers:\n";
     uint8_t bCntr = 0;
     for (auto bIt = buckets.begin(); bIt != buckets.end(); bIt++) {
       if (bIt->numOfKmers != 0) {
-        totalBits += bIt->seqLength*2 + ceil(log2(bIt->numOfKmers));
+        sumNumKmers += bIt->numOfKmers;
+        totalBits += bIt->seqLength*2 + bIt->numOfKmers*ceil(log2(bIt->numOfKmers));
         std::cout << "bucket" << static_cast<size_t>(bCntr++) << " : "
                 << "# of unitigs: " << bIt->numOfUnitigs << " , "
                 << "# of kmers: " << bIt->numOfKmers << " , "
                 << "bits per kmer pos "<< ceil(log2(bIt->numOfKmers)) << "\n";
       }
     }
-    std::cout <<"total bits: " << totalBits << " or " << totalBits/(1024*1024*8) << "MB\n";
+    std::cout <<"total number of kmers: " << sumNumKmers << "\n"
+              <<"total bits: " << totalBits << " or " << totalBits/(1024*1024*8) << "MB\n";
   }
 
 }
