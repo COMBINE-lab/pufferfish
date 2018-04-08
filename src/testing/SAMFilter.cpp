e97b20305d0053dbc2a158438a18c70716f6053b
Fatemeh <sfmodarres@gmail.com>
2018-04-08 13:15:48 -0400

    Added filtering all FPs to the SAMFilter

--- src/testing/SAMFilter.cpp
+++ src/testing/SAMFilter.cpp
@@ -9,44 +9,32 @@
 #include "SeqLib/BamHeader.h"
 #include "SeqLib/BamRecord.h"
 
-int main(int argc, char* argv[]) {
-    std::shared_ptr<spdlog::logger> logger = spdlog::stderr_color_mt("console");
-
-    if (argc < 4) {
-        logger->error("require at least 3 input arguments: input bam (str), output bam (str), filtering threshold (number between 0 and 1)");
-        exit(1);
+bool isTruth(SeqLib::BamRecord& rec, SeqLib::BamHeader& bh) {
+    if (rec.MappedFlag() or rec.PairMappedFlag()) { 
+        std::string gis = bh.IDtoName(rec.ChrID());
+        std::string readgis = rec.Qname();
+        size_t s = gis.find_first_of('|')+1;
+        size_t e = gis.find_first_of('|', s);
+        gis = gis.substr(s, e-s-1);
+        s = readgis.find_first_of('|')+1;
+        e = readgis.find_first_of('|', s);
+        readgis = readgis.substr(s, e-s-1);
+        return readgis == gis;
     }
-    logger->info("input: {}", argv[1]);
-    logger->info("output: {}", argv[2]);
-    logger->info("filter thresh: {}", argv[3]);
-    std::string mfileName = argv[1];
-    std::string moutfileName = argv[2];
-    float prob = std::stof(argv[3]);
-    SeqLib::BamReader br;
-    SeqLib::BamWriter bw;
+    return false;
+}
+void filterPartfp(SeqLib::BamReader& br,
+                 SeqLib::BamWriter& bw,
+                 SeqLib::BamHeader& bh,
+                 float prob,
+                 std::shared_ptr<spdlog::logger>& logger) {
+    uint64_t cntr = 0;
     SeqLib::BamRecord rec;
     SeqLib::BamRecord rec2;
-    br.Open(mfileName);   
-    bw.Open(moutfileName); 
-    SeqLib::BamHeader bh = br.Header();   
-    bw.SetHeader(br.Header());  
-    bw.WriteHeader();       
-    logger->info("# of targets: {}", bh.NumSequences());            
     
-    uint64_t cntr = 0;
     while (br.GetNextRecord(rec)) {        
         if ( (rec.MappedFlag() or rec.PairMappedFlag()) ) {
-            
-            std::string gis = bh.IDtoName(rec.ChrID());
-            std::string readgis = rec.Qname();
-            size_t s = gis.find_first_of('|')+1;
-            size_t e = gis.find_first_of('|', s);
-            gis = gis.substr(s, e-s-1);
-            s = readgis.find_first_of('|')+1;
-            e = readgis.find_first_of('|', s);
-            readgis = readgis.substr(s, e-s-1);
-    
-            if ( gis != readgis and ((double) rand() / (RAND_MAX)) <= prob ) {
+            if ( !isTruth(rec, bh) and ((double) rand() / (RAND_MAX)) <= prob ) {
                 if (rec.PairedFlag())
                     br.GetNextRecord(rec2);
                 continue;
@@ -59,9 +47,89 @@ int main(int argc, char* argv[]) {
             bw.WriteRecord(rec2);
             cntr++;
         }
-        if (cntr % 1000000 == 0) {
+        if (cntr % 10000000 == 0) {
             logger->info("{} records processed.", cntr);
         }
     }
+}
+
+void filterOnlytp(SeqLib::BamReader& br,
+                 SeqLib::BamWriter& bw,
+                 SeqLib::BamHeader& bh,
+                 std::shared_ptr<spdlog::logger>& logger) {
+    uint64_t cntr = 0;
+    SeqLib::BamRecord rec;
+    SeqLib::BamRecord rec2;
+    bool hasTruth = false;
+    std::string curReadName = "dummy";
+    SeqLib::BamRecordVector recvec;
+    recvec.reserve(3000);
+    while (br.GetNextRecord(rec)) {
+        cntr++;
+        if (rec.Qname() != curReadName) {
+            curReadName = rec.Qname();
+            if (hasTruth) {
+                for (auto& rec_it : recvec) {
+                    bw.WriteRecord(rec_it);
+                }
+            }
+            recvec.clear();
+            hasTruth = false;
+        }
+        if (isTruth(rec, bh)) {
+            hasTruth = true;
+        }        
+        recvec.push_back(rec);  
+        if (cntr % 10000000 == 0) {
+            logger->info("{} records processed.", cntr);
+        }           
+    }
+    // for last set of records
+    if (hasTruth) {
+        for (auto& rec_it : recvec) {
+            bw.WriteRecord(rec_it);
+        }
+    }
+    
+}
+
+int main(int argc, char* argv[]) {
+    std::shared_ptr<spdlog::logger> logger = spdlog::stderr_color_mt("console");
+    if (argc < 4) {
+        logger->error("choose one of the filter types 1. all_fps 2. part_fps\n and at least two args, input and output BAM file names");
+        exit(1);
+    }
+
+    std::string command = argv[1];
+    std::string mfileName = argv[2];
+    std::string moutfileName = argv[3];
+    
+    logger->info("command: {}", command);
+    logger->info("input: {}", mfileName);
+    logger->info("output: {}", moutfileName);
+
+    SeqLib::BamReader br;
+    SeqLib::BamWriter bw;
+    br.Open(mfileName);   
+    bw.Open(moutfileName); 
+    SeqLib::BamHeader bh = br.Header();   
+    bw.SetHeader(br.Header());  
+    bw.WriteHeader();       
+    logger->info("# of targets: {}", bh.NumSequences());            
+    
+    if (command == "all_fps") {
+        filterOnlytp(br, bw, bh, logger);    
+    }
+    else if (command == "part_fps") {
+        if (argc < 5) {
+            logger->error("require at least 3 input arguments: input bam (str), output bam (str), filtering threshold (number between 0 and 1)");
+            exit(1);
+        }
+        float prob = std::stof(argv[4]);
+        logger->info("filter thresh: {}", prob);    
+        filterPartfp(br, bw, bh, prob, logger); 
+    }
+    
+    br.Close();
     bw.Close();
 }
