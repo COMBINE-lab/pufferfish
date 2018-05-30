2cf62ef4a46e865499b0d504dddfbb37ca7cfbd7
Fatemeh <sfmodarres@gmail.com>
2018-05-30 16:32:04 -0400

    updated validation; added new metrics

--- scripts/validate_results.py
+++ scripts/validate_results.py
@@ -36,20 +36,26 @@ def calc_ref_metrics(truer, quants,  est_col, tru_col='cnt'):
                 "Mean Abs Error" : sklearn.metrics.mean_absolute_error, \
                 "Mean Sq. Error" : sklearn.metrics.mean_squared_error, \
                 "Mean Sq. Log. Error" : sklearn.metrics.mean_squared_log_error, \
-                "Med Abs Error" : sklearn.metrics.median_absolute_error}
+                "Med Abs Error" : sklearn.metrics.median_absolute_error, \
+                "Bray Curtis" : scipy.spatial.distance.braycurtis, \
+                "Kendall Tau" : scipy.stats.kendalltau, \
+                "Cosine Similarity" : scipy.spatial.distance.cosine, \
+#                "Minkowski Distance" : scipy.spatial.distance.minkowski, \
+                "Canberra Distance": scipy.spatial.distance.canberra}
 
     metric_res = []
     merged=pd.merge(truth, est, on='id', how='outer').fillna(0)
     MARD = mard(merged, tru_col, est_col)
     pcc = merged[[tru_col, est_col]].corr(method='pearson')[tru_col][est_col]
     sp = merged[[tru_col, est_col]].corr(method='spearman')[tru_col][est_col]
-    metric_res += [MARD, pcc, sp]
+    metric_dict = {'mard': MARD, 'pcc': pcc, 'sp': sp}
     for k, v in metrics.items():
         val = v(merged[tru_col], merged[est_col])
-        metric_res += [val]
-    df = pd.DataFrame(metric_res, columns=['metrics']).transpose()
-    
-    df.columns=['mard', 'pcc', 'sp', 'r2', 'ex_var', 'mae', 'mse', 'msle', 'medae']
+        if k == "Kendall Tau":
+            metric_dict[k] = val.correlation
+        else:
+            metric_dict[k] = val
+    df = pd.DataFrame(metric_dict, index=[0])
     merged.columns=['refid','truth', 'pred', 'ard']
     merged['abs_err'] = abs(merged['truth']-merged['pred'])
     return merged, df.transpose()
@@ -62,26 +68,38 @@ def calc_taxlevel_metrics(truer, est):
                 "Mean Abs Error" : sklearn.metrics.mean_absolute_error, \
                 "Mean Sq. Error" : sklearn.metrics.mean_squared_error, \
                 "Mean Sq. Log. Error" : sklearn.metrics.mean_squared_log_error, \
-                "Med Abs Error" : sklearn.metrics.median_absolute_error}
+                "Med Abs Error" : sklearn.metrics.median_absolute_error, \
+                "Bray Curtis" : scipy.spatial.distance.braycurtis, \
+                "Kendall Tau" : scipy.stats.kendalltau, \
+                "Cosine Similarity" : scipy.spatial.distance.cosine, \
+#                "Minkowski Distance" : scipy.spatial.distance.minkowski, \
+                "Canberra Distance": scipy.spatial.distance.canberra}
+
 
     metric_res = {}
     for r in ["Phylum", "Genus", "Species", "Scietific_Name", "TaxID"]:
-        metric_res[r] = []
+        metric_res[r] = {}
         tr_taxid_counts = true.groupby(r)['NumCounts'].sum().reset_index()
         es_taxid_counts = estimated.groupby(r)['NumCounts'].sum().reset_index()
         merged=pd.merge(tr_taxid_counts, es_taxid_counts, on=r, how='outer').fillna(0)
         MARD = mard(merged, 'NumCounts_x', 'NumCounts_y')
         pcc = merged[['NumCounts_x', 'NumCounts_y']].corr(method='pearson')['NumCounts_x']['NumCounts_y']
         sp = merged[['NumCounts_x', 'NumCounts_y']].corr(method='spearman')['NumCounts_x']['NumCounts_y']
-        metric_res[r] += [MARD, pcc, sp]
+        metric_res[r]['mard'] = MARD
+        metric_res[r]['pcc'] = pcc
+        metric_res[r]['sp'] = sp
+        #metric_res[r] += [MARD, pcc, sp]
         for k, v in metrics.items():
             val = v(merged['NumCounts_x'], merged['NumCounts_y'])
-            metric_res[r] += [val]
-        df = pd.DataFrame.from_dict(metric_res, orient='index')
-        df.columns=['mard', 'pcc', 'sp', 'r2', 'ex_var', 'mae', 'mse', 'msle', 'medae']
+            if k == "Kendall Tau":
+                metric_res[r][k] = val.correlation
+            else:
+                metric_res[r][k] = val
+        df = pd.DataFrame(metric_res)
+        #df.columns=['mard', 'pcc', 'sp', 'r2', 'ex_var', 'mae', 'mse', 'msle', 'medae']
         merged.columns=['taxid','truth', 'pred', 'ard']
         merged['abs_err'] = abs(merged['truth']-merged['pred'])
-    return merged, df.transpose()
+    return merged, df[['Phylum', 'Genus', 'Species', 'Scietific_Name', 'TaxID']]
 
 if args.truthType == 'transcriptome':
     d, m = calc_ref_metrics(args.truer, args.est, 'TPM')
