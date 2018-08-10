7bb0448980f2bac5082e084d07fab8d8588c644a
Rob Patro <rob@newton>
2018-08-10 10:41:33 -0400

    update ksw2

--- include/ksw2pp/KSW2Aligner.hpp
+++ include/ksw2pp/KSW2Aligner.hpp
@@ -3,9 +3,12 @@
 
 #include <memory>
 #include <vector>
+#include <cstdlib>
 
+extern "C" {
 #include "ksw2pp/kalloc.h"
 #include "ksw2pp/ksw2.h"
+}
 
 namespace ksw2pp {
 
@@ -35,6 +38,7 @@ struct KSW2Config {
   int dropoff = -1;
   int flag = 0;
   int alphabetSize = 5;
+  int end_bonus = 10;
   KSW2AlignmentType atype = KSW2AlignmentType::GLOBAL;
 };
 
@@ -44,6 +48,13 @@ public:
   KSW2Aligner(int8_t match = 2, int8_t mismatch = -4);
   KSW2Aligner(std::vector<int8_t> mat);
 
+  int transformSequenceKSW2(const char* const queryOriginal, const int queryLength,
+                            std::vector<unsigned char>& queryTransformed);
+
+  int transformSequencesKSW2(const char* const queryOriginal, const int queryLength,
+                             const char* const targetOriginal, const int targetLength,
+                             std::vector<unsigned char>& queryTransformed,
+                             std::vector<unsigned char>& targetTransformed);
   /**
    * Variants of the operator that require both an explicit type tag to
    * determine the type of alignment to perform, as well as a pointer to
@@ -108,12 +119,6 @@ public:
     }
   }
 
-  void freeCIGAR() {
-    if (result_.cigar and kalloc_allocator_) {
-      kfree(kalloc_allocator_.get(), result_.cigar);
-    }
-  }
-
 private:
   std::vector<uint8_t> query_;
   std::vector<uint8_t> target_;
@@ -122,6 +127,8 @@ private:
                                                          KallocDeleter()};
   std::vector<int8_t> mat_;
   KSW2Config config_;
+  bool haveSSE41{false};
+  bool haveSSE2{false};
 };
 } // namespace ksw2pp
 #endif //__KSW2_ALIGNER_HPP__
--- src/ksw2pp/KSW2Aligner.cpp
+++ src/ksw2pp/KSW2Aligner.cpp
@@ -1,7 +1,63 @@
-#include "KSW2Aligner.hpp"
-#include "ksw2.h"
+#include "ksw2pp/KSW2Aligner.hpp"
+#include <iostream>
+
+/*
+extern void ksw_extz2_sse2(void *km, int qlen, const uint8_t *query, int tlen, const uint8_t *target, int8_t m, const int8_t *mat, int8_t q, int8_t e, int w, int zdrop, int end_bonus, int flag, ksw_extz_t *ez);
+extern void ksw_extz2_sse41(void *km, int qlen, const uint8_t *query, int tlen, const uint8_t *target, int8_t m, const int8_t *mat, int8_t q, int8_t e, int w, int zdrop, int end_bonus, int flag, ksw_extz_t *ez);
+*/
 
 namespace ksw2pp {
+
+// The following is from ksw2_dispatch.c : https://github.com/lh3/minimap2/blob/master/ksw2_dispatch.c
+#define SIMD_SSE     0x1
+#define SIMD_SSE2    0x2
+#define SIMD_SSE3    0x4
+#define SIMD_SSSE3   0x8
+#define SIMD_SSE4_1  0x10
+#define SIMD_SSE4_2  0x20
+#define SIMD_AVX     0x40
+#define SIMD_AVX2    0x80
+#define SIMD_AVX512F 0x100
+
+#ifndef _MSC_VER
+// adapted from https://github.com/01org/linux-sgx/blob/master/common/inc/internal/linux/cpuid_gnu.h
+void __cpuidex(int cpuid[4], int func_id, int subfunc_id)
+{
+#if defined(__x86_64__)
+	asm volatile ("cpuid"
+			: "=a" (cpuid[0]), "=b" (cpuid[1]), "=c" (cpuid[2]), "=d" (cpuid[3])
+			: "0" (func_id), "2" (subfunc_id));
+#else // on 32bit, ebx can NOT be used as PIC code
+	asm volatile ("xchgl %%ebx, %1; cpuid; xchgl %%ebx, %1"
+			: "=a" (cpuid[0]), "=r" (cpuid[1]), "=c" (cpuid[2]), "=d" (cpuid[3])
+			: "0" (func_id), "2" (subfunc_id));
+#endif
+}
+#endif
+
+int x86_simd(void)
+{
+	int flag = 0, cpuid[4], max_id;
+	__cpuidex(cpuid, 0, 0);
+	max_id = cpuid[0];
+	if (max_id == 0) return 0;
+	__cpuidex(cpuid, 1, 0);
+	if (cpuid[3]>>25&1) flag |= SIMD_SSE;
+	if (cpuid[3]>>26&1) flag |= SIMD_SSE2;
+	if (cpuid[2]>>0 &1) flag |= SIMD_SSE3;
+	if (cpuid[2]>>9 &1) flag |= SIMD_SSSE3;
+	if (cpuid[2]>>19&1) flag |= SIMD_SSE4_1;
+	if (cpuid[2]>>20&1) flag |= SIMD_SSE4_2;
+	if (cpuid[2]>>28&1) flag |= SIMD_AVX;
+	if (max_id >= 7) {
+		__cpuidex(cpuid, 7, 0);
+		if (cpuid[1]>>5 &1) flag |= SIMD_AVX2;
+		if (cpuid[1]>>16&1) flag |= SIMD_AVX512F;
+	}
+	return flag;
+}
+// end of ksw2_dispatch.c here
+
 unsigned char seq_nt4_table_loc[256] = {
     0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
     4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
@@ -15,7 +71,10 @@ unsigned char seq_nt4_table_loc[256] = {
     4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
     4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
 
-KSW2Aligner::KSW2Aligner(int8_t match, int8_t mismatch) {
+  KSW2Aligner::KSW2Aligner(int8_t match, int8_t mismatch) {
+  unsigned int simd = x86_simd();
+  haveSSE41 = (simd & SIMD_SSE4_1);
+  haveSSE2 = (simd & SIMD_SSE2);
   query_.clear();
   target_.clear();
   kalloc_allocator_.reset(km_init());
@@ -38,6 +97,9 @@ KSW2Aligner::KSW2Aligner(int8_t match, int8_t mismatch) {
 }
 
 KSW2Aligner::KSW2Aligner(std::vector<int8_t> mat) {
+  unsigned int simd = x86_simd();
+  haveSSE41 = (simd & SIMD_SSE4_1);
+  haveSSE2 = (simd & SIMD_SSE2);
   query_.clear();
   target_.clear();
   kalloc_allocator_.reset(km_init());
@@ -65,8 +127,47 @@ KSW2Aligner::KSW2Aligner(std::vector<int8_t> mat) {
  * length - 1].
  * @return  Alphabet length - number of letters in recognized alphabet.
  */
-static int
-transformSequencesDNA(const char* const queryOriginal, const int queryLength,
+int
+KSW2Aligner::transformSequenceKSW2(const char* const queryOriginal, const int queryLength,
+                                   std::vector<unsigned char>& queryTransformed) {
+  // Alphabet is constructed from letters that are present in sequences.
+  // Each letter is assigned an ordinal number, starting from 0 up to
+  // alphabetLength - 1, and new query and target are created in which letters
+  // are replaced with their ordinal numbers. This query and target are used in
+  // all the calculations later.
+  queryTransformed.resize(queryLength, 0);
+
+  int i = 0;
+  for (int i = 0; i < queryLength; i++) {
+    uint8_t c = static_cast<uint8_t>(queryOriginal[i]);
+    queryTransformed[i] = seq_nt4_table_loc[c];
+  }
+  return 4;
+}
+
+/**
+ * from https://github.com/rob-p/edlib/blob/read-aligner/edlib/src/edlib.cpp
+ * Takes char query and char target, recognizes alphabet and transforms them
+ * into unsigned char sequences where elements in sequences are not any more
+ * letters of alphabet, but their index in alphabet. Most of internal edlib
+ * functions expect such transformed sequences. This function will allocate
+ * queryTransformed and targetTransformed, so make sure to free them when done.
+ * Example:
+ *   Original sequences: "ACT" and "CGT".
+ *   Alphabet would be recognized as ['A', 'C', 'T', 'G']. Alphabet length = 4.
+ *   Transformed sequences: [0, 1, 2] and [1, 3, 2].
+ * @param [in] queryOriginal
+ * @param [in] queryLength
+ * @param [in] targetOriginal
+ * @param [in] targetLength
+ * @param [out] queryTransformed  It will contain values in range [0, alphabet
+ * length - 1].
+ * @param [out] targetTransformed  It will contain values in range [0, alphabet
+ * length - 1].
+ * @return  Alphabet length - number of letters in recognized alphabet.
+ */
+int
+KSW2Aligner::transformSequencesKSW2(const char* const queryOriginal, const int queryLength,
                       const char* const targetOriginal, const int targetLength,
                       std::vector<unsigned char>& queryTransformed,
                       std::vector<unsigned char>& targetTransformed) {
@@ -110,16 +211,24 @@ int KSW2Aligner::operator()(const char* const queryOriginal,
   // auto ez = &result_;
   auto qlen = queryLength;
   auto tlen = targetLength;
-  int asize = transformSequencesDNA(queryOriginal, queryLength, targetOriginal,
+  int asize = transformSequencesKSW2(queryOriginal, queryLength, targetOriginal,
                                     targetLength, query_, target_);
   (void)asize;
-  int q = config_.gapo;
-  int e = config_.gape;
+  int8_t q = config_.gapo;
+  int8_t e = config_.gape;
   int w = config_.bandwidth;
   int z = config_.dropoff;
-  ksw_extz2_sse(kalloc_allocator_.get(), qlen, query_.data(), tlen,
+  if (haveSSE41) {
+    ksw_extz2_sse41(kalloc_allocator_.get(), qlen, query_.data(), tlen,
                 target_.data(), config_.alphabetSize, mat_.data(), q, e, w, z,
-                config_.flag, ez);
+                config_.end_bonus, config_.flag, ez);
+  } else if (haveSSE2) {
+    ksw_extz2_sse2(kalloc_allocator_.get(), qlen, query_.data(), tlen,
+                  target_.data(), config_.alphabetSize, mat_.data(), q, e, w, z,
+                  config_.end_bonus, config_.flag, ez);
+  } else {
+    std::abort();
+  }
   return ez->score;
 }
 
@@ -161,7 +270,7 @@ int KSW2Aligner::operator()(const char* const queryOriginal,
   // auto ez = &result_;
   auto qlen = queryLength;
   auto tlen = targetLength;
-  int asize = transformSequencesDNA(queryOriginal, queryLength, targetOriginal,
+  int asize = transformSequencesKSW2(queryOriginal, queryLength, targetOriginal,
                                     targetLength, query_, target_);
   (void)asize;
   int q = config_.gapo;
@@ -238,13 +347,21 @@ int KSW2Aligner::operator()(const uint8_t* const query_, const int queryLength,
                             EnumToType<KSW2AlignmentType::EXTENSION>) {
   auto qlen = queryLength;
   auto tlen = targetLength;
-  int q = config_.gapo;
-  int e = config_.gape;
+  int8_t q = config_.gapo;
+  int8_t e = config_.gape;
   int w = config_.bandwidth;
   int z = config_.dropoff;
-  ksw_extz2_sse(kalloc_allocator_.get(), qlen, query_, tlen, target_,
-                config_.alphabetSize, mat_.data(), q, e, w, z, config_.flag,
+  if (haveSSE41) {
+    ksw_extz2_sse41(kalloc_allocator_.get(), qlen, query_, tlen, target_,
+                config_.alphabetSize, mat_.data(), q, e, w, z, config_.end_bonus, config_.flag,
                 ez);
+  } else if (haveSSE2) {
+    ksw_extz2_sse2(kalloc_allocator_.get(), qlen, query_, tlen, target_,
+                  config_.alphabetSize, mat_.data(), q, e, w, z, config_.end_bonus, config_.flag,
+                  ez);
+  } else {
+    std::abort();
+  }
   return ez->score;
 }
 
