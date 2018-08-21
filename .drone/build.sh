306a96f9efc72adaae92ed584f72da60fbfc442a
avisrivastava <avi.srivastava@stonybrook.edu>
2018-08-20 23:47:50 -0400

    testin yum

--- .drone/build.sh
+++ .drone/build.sh
@@ -7,6 +7,10 @@ CPATH=`pwd`
 echo "[Drone build] current path : ${CPATH}"
 echo "[Drone build] making build directory"
 
+yum install -y --quiet wget
+wget http://download.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
+rpm -i --quiet epel-release-5-4.noarch.rpm
+
 mkdir build
 cd build
 
