024f51a1d8860d9bf7972de7a99228dca6b43ba1
avisrivastava <avi.srivastava@stonybrook.edu>
2018-08-20 23:54:09 -0400

    end to end

--- .drone/build.sh
+++ .drone/build.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-set -e
-
 source /hbb_exe/activate
 
-set -x
+set -e
 
 CPATH=`pwd`
 echo "[Drone build] current path : ${CPATH}"
+
+echo "[Drone build] Installikng sdsl"
+git clone https://github.com/simongog/sdsl-lite.git
+cd sdsl-lite
+./install.sh ../
+cd ..
+
 echo "[Drone build] making build directory"
-
-yum install -y --quiet wget
-wget http://download.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
-rpm -i --quiet epel-release-5-4.noarch.rpm
-
 mkdir build
 cd build
 
 echo "[Drone build] cmake configuration"
-
 cmake -DDO_QUIET_MAKE=TRUE ..
 
 echo "[Drone build] making pufferfish and installing locally (this could take a while)"
-
+make install
