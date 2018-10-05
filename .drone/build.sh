6e7082f8991b4026e5ae03967bda74688ae3430b
Fatemeh Almodaresi <sfmodarres@gmail.com>
2018-10-05 16:14:00 -0400

    we don't need complete seqLib library anymore.

--- .drone/build.sh
+++ .drone/build.sh
@@ -6,18 +6,6 @@ set -e
 CPATH=`pwd`
 echo "[Drone build] current path : ${CPATH}"
 
-# SeqLib, the c++ wrapper for htslib
-echo "[Drone build] maping seqLib"
-mkdir -p external_dependencies
-cd external_dependencies
-git clone --recursive https://github.com/walaj/SeqLib.git
-cd SeqLib
-./configure
-make CXXFLAGS='-std=c++11'
-make install
-make seqtools
-cd ../..
-
 echo "[Drone build] Installikng sdsl"
 git clone https://github.com/simongog/sdsl-lite.git
 cd sdsl-lite
