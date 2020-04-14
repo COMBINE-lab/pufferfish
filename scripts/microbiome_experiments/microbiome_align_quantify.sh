9340cc26dfef737b74887b9a24829ceafb143da2
Fatemeh Almodaresi <sfmodarres@gmail.com>
2020-04-13 21:35:18 -0400

    advanced: updated align_quantify script

--- scripts/microbiome_experiments/microbiome_align_quantify.sh
+++ scripts/microbiome_experiments/microbiome_align_quantify.sh
@@ -1,5 +1,8 @@
 root="/mnt/scratch3/SubmitPuffalign"
 
+mkdir -p ${root}/alignments/puffaligner
+mkdir -p ${root}/alignments/bowtie2
+
 #names=( 'primary' 'primary.allowDove' 'maxNumHits20' 'maxNumHits20.allowDove' )
 #puffOpts=( '--noDovetail --bestStrata --primaryAlignment' '--bestStrata --primaryAlignment' '--noDovetail --maxNumHits 20' '--maxNumHits 20' )
 #bw2Opts=( '-k 1' '-k 1 --dovetail' '-k 20' '-k 20 --dovetail' )
@@ -24,7 +27,7 @@ for dataType in 'covid19' 'sars' 'bat2008' ; do
 		name="${root}/alignments/puffaligner/${dataType}.pe.concordant.${names[$idx]}"
 		cmd="/usr/bin/time /home/mohsen/Pufferfish-repo/pufferfish/build/src/pufferfish align -i ${root}/indices/pufferfishindices/viralCG.puffindex/ -1 ${root}/data/microbiome_simulation/${dataType}_apr9_1.fastq -2 ${root}/data/microbiome_simulation/${dataType}_apr9_2.fastq -o ${name}.sam  --noOrphans --noDiscordant --noDovetail ${puffOpts[$idx]} -t 16 > ${name}.log 2>&1"
 		echo ${cmd}
-#		eval ${cmd}
+		eval ${cmd}
 		echo ${cmd} >> ${name}.log
 		echo ""
 	
@@ -33,7 +36,7 @@ for dataType in 'covid19' 'sars' 'bat2008' ; do
 		name="${root}/alignments/bowtie2/${dataType}.pe.concordant.${names[$idx]}"
 		cmd="/usr/bin/time ${root}/tools/bowtie2-2.3.5.1-linux-x86_64/bowtie2 ${bw2Opts[$idx]} --gbar 1 --no-discordant --no-mixed  -x ${root}/indices/bowtie2indices/viralCG.bowtie2index/bw2 -1 ${root}/data/microbiome_simulation/${dataType}_apr9_1.fastq -2 ${root}/data/microbiome_simulation/${dataType}_apr9_2.fastq -S ${name}.sam -p 16 --ignore-quals --maxins 1000 > ${name}.log 2>&1"
 		echo ${cmd}
-#		eval ${cmd}
+		eval ${cmd}
 		echo ${cmd} >> ${name}.log
 
 		echo "-------------------"
@@ -59,10 +62,10 @@ for idx in "${!names[@]}"; do
 
 	echo "Puffaligner"
 	name="${root}/alignments/puffaligner/${dataType}.pe.concordant.${names[$idx]}"
-	cmd="/usr/bin/time /home/mohsen/Pufferfish-repo/pufferfish/build/src/pufferfish align -i ${root}/indices/pufferfishindices/microbiome_k31.resubmission.puffindex.sparse/ -1 ${r1} -2 ${r2} -o ${name}.sam  --noOrphans --noDiscordant ${puffOpts[$idx]} -t 16 > ${name}.log 2>&1"
+	cmd="/usr/bin/time /home/mohsen/Pufferfish-repo/pufferfish/build/src/pufferfish align -i ${root}/indices/pufferfishindices/microbiome_k31.resubmission.puffindex.sparse/ -1 ${r1} -2 ${r2} -o ${name}.sam  --noOrphans --noDiscordant --noDovetail ${puffOpts[$idx]} -t 16 > ${name}.log 2>&1"
 	echo ${cmd}
-#eval ${cmd}
-#	echo ${cmd} >> ${name}.log
+	eval ${cmd}
+	echo ${cmd} >> ${name}.log
 	echo ""
 ########## salmon ##########
 	echo "Salmon"
@@ -70,8 +73,8 @@ for idx in "${!names[@]}"; do
 	mkdir -p ${quantname}
 	cmd="/usr/bin/time ${root}/tools/salmon-latest_linux_x86_64/bin/salmon quant -p 16 -la -a ${name}.sam -t ${root}/microbiome/bacteria_virus/library/bacteria/selected_complete_genome.fna -o ${quantname} 2>${quantname}/log"
 	echo ${cmd}
-#	eval ${cmd}
-#	echo ${cmd} >> ${quantname}/log
+	eval ${cmd}
+	echo ${cmd} >> ${quantname}/log
 
 
 
@@ -80,8 +83,8 @@ for idx in "${!names[@]}"; do
 	name="${root}/alignments/bowtie2/${dataType}.pe.concordant.${names[$idx]}"
 	cmd="/usr/bin/time ${root}/tools/bowtie2-2.3.5.1-linux-x86_64/bowtie2 ${bw2Opts[$idx]} --gbar 1 --no-discordant --no-mixed  -x ${root}/indices/bowtie2indices/microbiome.bowtie2index/bw2 -1 ${r1} -2 ${r2} -S ${name}.sam -p 16 --ignore-quals --maxins 1000 > ${name}.log 2>&1"
 	echo ${cmd}
-#eval ${cmd}
-#	echo ${cmd} >> ${name}.log
+	eval ${cmd}
+	echo ${cmd} >> ${name}.log
 	echo ""
 
 ######### salmon ##########
